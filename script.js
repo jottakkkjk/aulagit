d82f544df673b33a6e05a31a494b86322dd64da8
jottakkkjk <joaopedrosoarespereira1801@gmail.com>
2024-09-04 16:39:59 -0300

    Alteramento

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')()
 const n1 = Number(prompt('Digite um numero: '))
 const n2 = Number(prompt('digite outro numero: '))
-const soma = n1 + n2
-console.log(soma)
+const mult = n1 * n2
+console.log(mult)
