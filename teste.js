693a3a669199dcb411283f32d9b4cada60f2a69b
GustavoGPQ <gustavoporto0711@gmail.com>
2024-09-04 15:23:07 -0300

    teste de enviar

---
+++ teste.js
@@ -0,0 +1,12 @@
+// Exibe uma mensagem de alerta
+alert("Olá, mundo!");
+
+// Declara duas variáveis
+var numero1 = 5;
+var numero2 = 10;
+
+// Realiza uma soma
+var soma = numero1 + numero2;
+
+// Exibe o resultado no console
+console.log("A soma de " + numero1 + " e " + numero2 + " é " + soma);
